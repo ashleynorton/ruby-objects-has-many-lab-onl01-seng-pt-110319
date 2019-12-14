5fe1925c2155898ed68cbfc84d973d264a45812b
Ashley Norton <nortonashleym@gmail.com>
2019-12-14 18:39:19 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,8 +1,6 @@
 class Artist 
   attr_accessor :name
   
-  @@song_count = 0 
-  
   def initialize(name)
     @name = name 
     @songs = [] 
@@ -17,7 +15,6 @@ class Artist
   def add_song(song)
     song.artist = self 
     @songs << song
-    @@song_count += 1 
   end 
   
   def add_song_by_name(song_name)
